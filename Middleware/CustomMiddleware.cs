d65764945f58790f9dfda36fe5613ab0f2ef6827
Karolina Jakubowska <kuki2000j@icloud.com>
2022-05-13 10:19:49 +0200

    Dodaj pliki projektów.

---
+++ Middleware/CustomMiddleware.cs
@@ -0,0 +1,33 @@
+﻿namespace Zadanie8.Middleware
+{
+    public class CustomMiddleware
+    {
+        private RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            string userAgent = context.Request.Headers["User-Agent"].ToString();
+            var keyWords = new[] { "MSIE", "Trident","Edg" };
+
+            if (keyWords.Any(userAgent.Contains))
+            {
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+            }
+            return _next(context);
+        }
+    }
+
+    public static class MyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
+
